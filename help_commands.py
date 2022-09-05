c2628f8e7614b299c1315a80627f97c6dd34cc85
Hayden <hd.rydell@outlook.com>
2022-09-05 16:41:51 +1000

    Added server validation and help command group

---
+++ help_commands.py
@@ -0,0 +1,4 @@
+BANNER_DESC = "This module can help servers manipulate their banner through commands. Servers naturally must have access to this feature before use."
+BANNER_HELP = """
+
+"""
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import help_commands
 import os
 from discord.ext    import commands
 from dotenv         import load_dotenv
@@ -6,6 +7,7 @@ from dotenv         import load_dotenv
 # Bot setup
 load_dotenv("data/.env")
 TOKEN = os.getenv("TOKEN")      # Your unique Bot Token
+LOCAL_BANNER_PATH = "/banners/"
 BOT = commands.Bot(command_prefix = ".", description = "ggnosis-dev was here")     # change prefix for preference
 BOT.remove_command("help")
 
@@ -19,8 +21,24 @@ async def user_validation(ctx):
         return False
     return True
 
+# Server validations
+async def server_validation(ctx):
+    if ctx.message.guild.premium_tier < 2:
+        await ctx.send("This server does not have access to a banner (Need Nitro level 2).")
+        await ctx.message.add_reaction("❌")
+        return False
+    return True
+
+# Events
 @BOT.event
 async def on_ready():
     await BOT.change_presence(activity = discord.Game(name = "ggnosis-dev was here"))
     print("Logged in: ", BOT.user)
 
+# Help commands
+@BOT.group(name = "banner", invoke_without_command = True)
+async def banners_commands(ctx, option : str = "help"):
+    if option == "help":
+        help_msg = discord.Embed(title = "▬▬ **__BANNER COMMANDS__** ▬▬", colour = 0x970A3D, description = help_commands.BANNER_DESC)
+        help_msg.set_footer(text = help_commands.BANNER_HELP)
+        await ctx.send(embed = help_msg)
