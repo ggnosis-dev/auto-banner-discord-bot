45a8e0a2b45b068ba0261ee3092a256461d2bf2f
Hayden <hd.rydell@outlook.com>
2022-09-07 14:14:35 +1000

    Added information to help_commands

--- help_commands.py
+++ help_commands.py
@@ -1,4 +1,12 @@
 BANNER_DESC = "This module can help servers manipulate their banner through commands. Servers naturally must have access to this feature before use."
 BANNER_HELP = """
+Post server banner in chat:
+    .banner get
 
+Set server banner
+    .banner set {<link> | [--random | -r]}
+    
+    OPTIONS:
+    <link> : Set banner to a linked image
+    --random | -r : Set to a random image found in server's gallery
 """
