2df35916e7d998acb332b9165fe82abe751cc1bf
Hayden <hd.rydell@outlook.com>
2022-09-13 15:56:30 +1000

    Added to help messages

--- help_commands.py
+++ help_commands.py
@@ -4,22 +4,32 @@ Post server banner in chat:
     .banner get
 
 Set server banner
-    .banner set {<link> | [--random | -r]}
+    .banner set {<link> | <number> | [--random | -r]}
     
     OPTIONS:
     <link> : Set banner to a linked image
+    <number> : Set banner to image in banner gallery at index
     --random | -r : Set to a random image found in server's gallery
 
 Start the banner cycle:
-    .banner start
+    .banner start {<None> | [--asc | -a] | [--dsc | -d] | [--random | -r]}
+
+    OPTIONS:
+    <None> : Default to random cycle
+    --asc | -a : Cycles through banner folder images in ascending order
+    --dsc | -d : Cycles through banner folder images in descending order
+    --random | -r : Cycles through banner folder images at random
 
 Stop the banner cycle:
     .banner stop
 
-Update how long it takes for banner to cycle to the next. NOTE: Discord may limit data transfer if made too frequent, would recommend at least 20 minutes between cycles.
+Update how long it takes for banner to cycle to the next (**NOTE:** Discord may limit data transfer if made too frequent, would recommend at least 20 minutes between cycles): 
     .banner timer {<None> | [--set | -s <minutes>]}
 
     OPTIONS:
-    <None> : Shows the current timer
-    --set | -s <minutes> : Set the timer 
+    <None> : Shows the current set timer
+    --set | -s <minutes> : Set the timer
+
+Check when next cycle will be:
+    .banner next
 """
